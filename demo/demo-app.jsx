7c3741c41f6551ee47d6a56f77ed0cd378fa07ea
PanSpagetka <rknaur@redhat.com>
2018-03-06 12:53:18 +0100

    Extract tags definition, change Demo and Story, use default state

--- demo/demo-app.jsx
+++ demo/demo-app.jsx
@@ -1,13 +1,25 @@
 import * as ReactDOM from 'react-dom';
 import * as React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import TaggingConnected from '../src/tagging/containers/tagging';
 import taggingApp from '../src/tagging/reducers';
 
-export default function renderApp() {
-  const store = createStore(taggingApp);
+const tags = {
+  Name: ['Pepa', 'Franta', 'Karel'],
+  Number: ['1', '2', '3', '4', '5'],
+  Animal: ['Duck', 'Pig', 'Dog', 'Unicorn'],
+  Food: ['Steak', 'Duck', 'Knedlik', 'Parek'],
+  Something: ['Steak', 'Duck', 'Veeeeery loooong teeeeeext heeeerreeee', 'Blabla'],
+};
 
+const setTags = [{ tagCategory: "Name", tagValue: "Pepa" }];
+const defaultState = { tags: tags, setTags: setTags };
+
+function a() { return { taggingApp: { setTags: { tagCategory: "Name", tagValue: "Pepa" }}}}
+export default function renderApp() {
+
+  const store = createStore(taggingApp, defaultState);
   ReactDOM.render(
     <Provider store={store}>
       <TaggingConnected />
