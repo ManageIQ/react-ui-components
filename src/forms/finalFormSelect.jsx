54bf4918f27a0ad0b4b8ad7c2dc09cbe1ac44a7b
Martin Maroši <marvusm.mmi@gmail.com>
2018-05-16 09:23:00 +0200

    Removed redundand component

--- src/forms/finalFormSelect.jsx
+++ src/forms/finalFormSelect.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormGroup, HelpBlock, Col } from 'patternfly-react';
-import Select from './select';
+import ReactSelect from 'react-select';
 import { inputObjectProps, metaObjectProps } from './finalFormPropTypes';
 import './finalFormSelectStyle.scss';
 
@@ -11,18 +11,31 @@ const FinalFormSelect = ({
   label,
   options,
   placeholder,
+  clearable,
   ...rest
-}) => (
-  <FormGroup validationState={meta.error ? 'error' : null}>
-    <Col xs={2} componentClass="label" className="control-label">
-      {label}
-    </Col>
-    <Col xs={8}>
-      <Select options={options} {...input} {...rest} hasError={!!meta.error} placeholder={placeholder} />
-      {meta.error && <HelpBlock>{meta.error}</HelpBlock>}
-    </Col>
-  </FormGroup>
-);
+}) => {
+  const { onChange, ...inputProps } = input;
+  return (
+    <FormGroup validationState={meta.error ? 'error' : null}>
+      <Col xs={2} componentClass="label" className="control-label">
+        {label}
+      </Col>
+      <Col xs={8}>
+        <ReactSelect
+          className={`${meta.error ? 'has-error' : ''} final-form-select`}
+          options={options}
+          clearable={clearable}
+          searchable={false}
+          placeholder={placeholder}
+          onChange={({ value }) => input.onChange(value)}
+          {...inputProps}
+          {...rest}
+        />
+        {meta.error && <HelpBlock>{meta.error}</HelpBlock>}
+      </Col>
+    </FormGroup>
+  );
+};
 
 FinalFormSelect.propTypes = {
   input: inputObjectProps,
@@ -33,10 +46,12 @@ FinalFormSelect.propTypes = {
     label: PropTypes.string.isRequired,
   })).isRequired,
   placeholder: PropTypes.string,
+  clearable: PropTypes.bool,
 };
 
 FinalFormSelect.defaultProps = {
   placeholder: 'Select',
+  clearable: false,
 };
 
 export default FinalFormSelect;
