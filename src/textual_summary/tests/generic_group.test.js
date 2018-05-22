8819fa671186118390e8806518c90eddb4f7cad3
Martin Povolny <mpovolny@redhat.com>
2018-05-22 14:59:58 +0200

    Fix and add tests for GenericGroup.

--- src/textual_summary/tests/generic_group.test.js
+++ src/textual_summary/tests/generic_group.test.js
@@ -9,6 +9,12 @@ describe('GenericGroup', () => {
   it('renders title and rows', () => {
     const wrapper = shallow(<GenericGroup items={genericGroupData.items} title={genericGroupData.title} onClick={e => null} />);
     expect(wrapper.html()).toContain(genericGroupData.title);
-    expect(wrapper.find(GenericTableRow).length).toEqual(1);
+    expect(wrapper.find(GenericTableRow)).toHaveLength(2);
+  });
+
+  it('renders just fine', () => {
+    const component = renderer.create(<GenericGroup items={genericGroupData.items} title={genericGroupData.title} onClick={e => null} />);
+    const group = component.toJSON();
+    expect(group).toMatchSnapshot();
   });
 });
