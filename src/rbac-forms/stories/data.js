0928bc0a125a6083646b5e303ac66721bc751c97
Martin Maroši <marvusm.mmi@gmail.com>
2018-06-26 10:20:37 +0200

    Moved stories data to separate file

---
+++ src/rbac-forms/stories/data.js
@@ -0,0 +1,42 @@
+export const groups = [
+  {
+    value: 1,
+    label: 'Hope',
+  },
+  {
+    value: 2,
+    label: 'Christa',
+  },
+  {
+    value: 3,
+    label: 'Tania',
+  },
+  {
+    value: 4,
+    label: 'Mcintyre',
+  },
+  {
+    value: 5,
+    label: 'Tonya',
+  },
+  {
+    value: 6,
+    label: 'Marcie',
+  },
+  {
+    value: 7,
+    label: 'Cara',
+  },
+  {
+    value: 8,
+    label: 'Becker',
+  },
+  {
+    value: 9,
+    label: 'Foley',
+  },
+  {
+    value: 10,
+    label: 'Perkins',
+  },
+];
--- src/rbac-forms/stories/rbacUserForm.stories.js
+++ src/rbac-forms/stories/rbacUserForm.stories.js
@@ -2,51 +2,9 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withInfo } from '@storybook/addon-info';
-
 import { RbacUserForm, RbacUserPreview } from '../';
+import { groups } from './data';
 
-const groups = [
-  {
-    value: 1,
-    label: 'Hope',
-  },
-  {
-    value: 2,
-    label: 'Christa',
-  },
-  {
-    value: 3,
-    label: 'Tania',
-  },
-  {
-    value: 4,
-    label: 'Mcintyre',
-  },
-  {
-    value: 5,
-    label: 'Tonya',
-  },
-  {
-    value: 6,
-    label: 'Marcie',
-  },
-  {
-    value: 7,
-    label: 'Cara',
-  },
-  {
-    value: 8,
-    label: 'Becker',
-  },
-  {
-    value: 9,
-    label: 'Foley',
-  },
-  {
-    value: 10,
-    label: 'Perkins',
-  },
-];
 const user = {
   name: 'Administrator',
   userid: 'Admin',
@@ -71,6 +29,7 @@ const user = {
     onClick: () => action('Current role click'),
   },
 };
+
 storiesOf('Rbac forms', module).add('Rbac add user form', withInfo()(() => (
   <RbacUserForm
     groups={groups}
