c15b2c14da8dc5fc97b18f9ecc9cf698ce49046c
Martin Maroši <marvusm.mmi@gmail.com>
2018-07-10 15:57:07 +0200

    Added exports for POC

--- src/rbac-forms/index.js
+++ src/rbac-forms/index.js
@@ -1,2 +1,8 @@
 export { default as RbacUserForm } from './rbacUserForm';
 export { default as RbacUserPreview } from './rbacUserPreview';
+export { default as usersReducer } from './rbacUserManagementModule/redux/reducers/usersReducer';
+export { default as UserAdd } from './rbacUserManagementModule/pages/userAdd';
+export { default as UserEdit } from './rbacUserManagementModule/pages/userEdit';
+export { default as UserPreview } from './rbacUserManagementModule/pages/userPreview';
+export { default as UserUsersList } from './rbacUserManagementModule/pages/usersList';
+export * from './rbacUserManagementModule/redux/actions/actions';
