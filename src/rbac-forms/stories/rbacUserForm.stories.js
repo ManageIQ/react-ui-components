3c7c3d3b4587f38ed7482bc9c31557a32d975399
Martin Maroši <marvusm.mmi@gmail.com>
2018-08-14 13:02:05 +0200

    Added storybook for Rbac assing user tags component

--- src/rbac-forms/stories/rbacUserForm.stories.js
+++ src/rbac-forms/stories/rbacUserForm.stories.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withInfo } from '@storybook/addon-info';
-import { RbacUserForm, RbacUserPreview } from '../';
+import { RbacUserForm, RbacUserPreview, RbacAssignCompanyTags } from '../';
 import { GenericPreviewTable } from '../../table/';
 import { groups, usersTableColumns, usersTableRows, tags } from './data';
 import RbacUserTagsList from '../rbacUserTagsList';
+import { users, categories } from './usersData';
 
 const user = {
   name: 'Administrator',
@@ -56,4 +57,36 @@ storiesOf('Rbac forms', module).add('Rbac add user form', withInfo()(() => (
 )))
   .add('Rbac user tags list', withInfo()(() => (
     <RbacUserTagsList tags={tags} tenant="My tenant" />
-  )));
+  )))
+  .add('Rbac assign user tags', withInfo()(() => {
+    const mockLoadCategoryEntry = () => new Promise(resolve => setTimeout(() => resolve([{
+      value: 1,
+      label: 'Label 1',
+    }, {
+      value: 2,
+      label: 'Label 2',
+    }, {
+      value: 3,
+      label: 'Label 3',
+    }, {
+      value: 10000000000092,
+      label: 'lazy loaded',
+    }]), 1500));
+    const mockLoadMultipleEntries = () => new Promise(resolve => setTimeout(() => resolve({
+      10000000000085: [{
+        value: 10000000000092,
+        label: 'lazy loaded',
+      }],
+    })), 1500);
+    return (
+      <RbacAssignCompanyTags
+        users={users}
+        columns={usersTableColumns}
+        categories={categories.map(category => ({ ...category, label: category.description, value: category.id }))}
+        loadCategoryEntry={mockLoadCategoryEntry}
+        loadMultipleEntries={mockLoadMultipleEntries}
+        handleCancel={action('Handle cancel')}
+        handleSave={action('Handle save')}
+      />
+    );
+  }));
