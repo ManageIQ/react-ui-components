5c8af1c253c6b442b65277d79ff4e91817f2667b
Martin Maroši <marvusm.mmi@gmail.com>
2018-08-29 10:35:33 +0200

    Fix rbacUserForm template to match miq layout.

--- src/rbac-forms/rbacUserForm.jsx
+++ src/rbac-forms/rbacUserForm.jsx
@@ -36,7 +36,7 @@ const RbacUserForm = ({
       form: { change, reset },
     }) => (
       <PfForm horizontal>
-        <Grid>
+        <Grid fluid>
           <Row>
             <Col xs={12}>
               <Field
