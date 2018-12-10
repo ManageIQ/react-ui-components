f7005895d1d09d71f8213f3e44fbdba5574b904e
PanSpagetka <rknaur@redhat.com>
2018-12-10 10:00:31 +0100

    Update Storybook

--- src/expression-editor/stories/ExpresionEditor.stories.js
+++ src/expression-editor/stories/ExpresionEditor.stories.js
@@ -3,13 +3,8 @@ import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
 import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
-// import MockExpressionEditor from '../components/MOCKS/MockExpressionEditor'
-// import { ExpressionEditorConnected } from '../containers/ExpressionEditor'
 import { ExpressionEditorConnected2 } from '../containers/ExpressionEditor2'
 import ExpressionEditorReducers from '../reducers/'
-import MockEditableChip from '../components/MOCKS/MockEditableChip'
-import MockExpression from '../components/MOCKS/MockExpression'
-import MockExpressionEditor2 from '../components/MOCKS/MockExpressionEditor2'
 import './index.scss';
 import { logicalOperatorsMock, userInputMock } from "../constants"
 
@@ -37,7 +32,10 @@ let defaultOptions = {id: 0, label: 'root', type: 'root', next:
           next: [{ id: 211, label: '=', type: 'operator', next: userInputMock}, { id: 212, label: '!=', type: 'operator', next: userInputMock}]
         },
         { id: 22, label: 'Department', type: 'category',
-          next: [{ id: 221, label: '=', type: 'operator', next: userInputMock}, { id: 222, label: '!=', type: 'operator', next: userInputMock}]
+          next: [{ id: 221, label: '=', type: 'operator', next: [{ id: 2211, label: 'Financial Services', type: 'value', next: []},
+            { id: 2212, label: 'Human Resources', type: 'value', next: []}]},
+          { id: 222, label: '!=', type: 'operator', next: [{ id: 2221, label: 'Financial Services', type: 'value', next: []},
+            { id: 2222, label: 'Human Resources', type: 'value', next: []}]}]
         },
         { id: 23, label: 'Environment', type: 'category',
           next: [{ id: 231, label: '=', type: 'operator', next: userInputMock}, { id: 232, label: '!=', type: 'operator', next: userInputMock}]
@@ -50,11 +48,5 @@ let defaultOptions = {id: 0, label: 'root', type: 'root', next:
   ]
 };
 
-storiesOf('ExpressionEditor2', module)
-  .add('ExpressionEditor2 MOCK', () => (<MockExpressionEditor2 />));
-
-storiesOf('ExpressionEditor2', module)
-  .add('ExpressionEditor2 CONNECTED', () => (<Provider store={store}><ExpressionEditorConnected2 data={defaultOptions}/></Provider>));
-
-storiesOf('Editable Chip', module)
-  .add('EditableChip', () => (<MockEditableChip />));
+storiesOf('Expression Editor', module)
+  .add('New Expression Editor', () => (<Provider store={store}><ExpressionEditorConnected2 data={defaultOptions}/></Provider>));
