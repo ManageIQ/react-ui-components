33036c05b6df65aef8579d498f8dd8e50cd78940
Martin Povolny <mpovolny@redhat.com>
2019-02-15 14:11:41 +0100

    Add tests for quadicons.

---
+++ src/quadicon/tests/quadicon.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import toJson from 'enzyme-to-json';
+import { mount } from 'enzyme';
+
+import { Quadicon } from '..';
+import { quads, numbers, piecharts, hosts, vms, containerproviders } from '../data/quadicons';
+
+/* eslint no-bitwise: ["error", { "allow": ["^", ">>>"] }], react/jsx-closing-tag-location: ["off"] */
+
+const hObj = (obj) => {
+  const s = JSON.stringify(obj);
+  let h = 1;
+  for (let i = 0; i < s.length; i += 1) {
+    h = Math.imul(h ^ s.charCodeAt(i), 951274213);
+  }
+  return (h ^ h >>> 9) >>> 0;
+};
+
+describe('Quadicon', () => {
+  it('renders just fine...', () => {
+    const all = [...quads, ...numbers, ...piecharts, ...hosts, ...vms, ...containerproviders];
+    const qs = mount(<React.Fragment>
+      { all.map(quad => <Quadicon key={hObj(quad)} data={quad} />)}
+    </React.Fragment>);
+    expect(toJson(qs)).toMatchSnapshot();
+  });
+});
