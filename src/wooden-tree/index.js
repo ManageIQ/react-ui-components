a31fc602c1bf8146eab90c6d272589c432e2ae23
Levente Berky <lberky@redhat.com>
2019-06-10 15:37:42 +0300

    Reexported react-wooden-tree with redux in PF style

---
+++ src/wooden-tree/index.js
@@ -0,0 +1,7 @@
+import { Tree } from 'react-wooden-tree';
+
+export { ActionTypes, callBack, Node, treeDataReducer } from 'react-wooden-tree';
+require('./index.scss');
+
+class WoodenTreeView extends Tree {}
+export default WoodenTreeView;
