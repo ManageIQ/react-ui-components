5b272f42589ee549e028aac290e723185fffaf63
Filip Hlavac <Filip@tkdelfin.cz>
2019-06-13 11:47:46 +0200

    Added array.prototype.flat to setupTests.js

--- scripts/setupTests.js
+++ scripts/setupTests.js
@@ -10,3 +10,13 @@ configure({ adapter: new Adapter() });
 const dom = new JSDOM();
 global.document = dom.window.document;
 global.d3 = d3;
+
+// mock flat
+if (!Array.prototype.flat) {
+  Object.defineProperty(Array.prototype, 'flat', { // eslint-disable-line no-extend-native
+    value(depth = 1) {
+      return this.reduce((flat, toFlatten) =>
+        flat.concat((Array.isArray(toFlatten) && (depth > 1)) ? toFlatten.flat(depth - 1) : toFlatten), []);
+    },
+  });
+}
