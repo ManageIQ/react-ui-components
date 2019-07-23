bbb68b905e0b004759c0fbf44aacc5cf90543fd6
Levente Berky <lberky@redhat.com>
2019-07-23 08:54:49 +0200

    Changed require to import statement for sass file

--- src/wooden-tree/index.js
+++ src/wooden-tree/index.js
@@ -1,7 +1,7 @@
 import { Tree } from 'react-wooden-tree';
+import './index.scss';
 
 export { ActionTypes, callBack, Node, treeDataReducer } from 'react-wooden-tree';
-require('./index.scss');
 
 class WoodenTreeView extends Tree {}
 export default WoodenTreeView;
