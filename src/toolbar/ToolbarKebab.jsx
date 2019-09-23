3a6787347c96af1fb18e93ef538e1daa3f18dbbf
Martin Povolny <mpovolny@redhat.com>
2019-09-23 15:22:51 +0200

    Toolbar: fix opening of multiple items in kebab list.
    Fix according to @rvsia.
    Requires https://github.com/patternfly/patternfly-react/pull/2984

--- src/toolbar/ToolbarKebab.jsx
+++ src/toolbar/ToolbarKebab.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { DropdownButton, MenuItem } from 'patternfly-react';
-import HorizontalNavMenuItem from '../../node_modules/patternfly-react/dist/js/components/HorizontalNav/HorizontalNavMenuItem';
+import { DropdownButton, MenuItem, HorizontalNavMenuItem } from 'patternfly-react';
 import { ToolbarClick } from './ToolbarClick';
 import './styles.scss';
 
@@ -23,6 +22,10 @@ const ToolbarSubmenu = props => (
     }
     dropdown
     submenu
+    onMouseEnter={() => props.setIsOpenId(props.id)}
+    onItemClick={() => props.setIsOpenId(props.id)}
+    onToggle={() => null}
+    open={props.openId === props.id}
   >
     { props.items.filter(i => !i.hidden).map(i => (
       <MenuItem
@@ -44,15 +47,17 @@ ToolbarSubmenu.propTypes = {
   icon: PropTypes.string,
   hidden: PropTypes.bool,
   onClick: PropTypes.func.isRequired,
+  openId: PropTypes.string,
+  setIsOpenId: PropTypes.func.isRequired,
 };
 
-const KebabListItem = (item, onClick) => {
+const KebabListItem = (item, onClick, openId, setIsOpenId) => {
   if (item.type === 'separator') {
     return <MenuItem key={item.id} disabled={!item.enabled} eventKey={item.id} divider />;
   }
 
   if (item.type === 'buttonSelect') {
-    return <ToolbarSubmenu key={item.id} {...item} onClick={onClick} />;
+    return <ToolbarSubmenu key={item.id} {...item} onClick={onClick} onSelect={console.log} openId={openId} setIsOpenId={setIsOpenId} />;
   }
 
   return (
@@ -62,13 +67,17 @@ const KebabListItem = (item, onClick) => {
   );
 };
 
-export const ToolbarKebab = props => (
-  <div className="kebab">
-    <DropdownButton id="menu_kebab" title={<span className="fa fa-ellipsis-v" />}>
-      {props.items.map(item => KebabListItem(item, props.onClick))}
-    </DropdownButton>
-  </div>
-);
+export const ToolbarKebab = (props) => {
+  const [openId, setIsOpenId] = useState(undefined);
+
+  return (
+    <div className="kebab">
+      <DropdownButton id="menu_kebab" title={<span className="fa fa-ellipsis-v" />}>
+        {props.items.map(item => KebabListItem(item, props.onClick, openId, setIsOpenId))}
+      </DropdownButton>
+    </div>
+  );
+};
 
 ToolbarKebab.propTypes = {
   items: PropTypes.arrayOf(PropTypes.any),
