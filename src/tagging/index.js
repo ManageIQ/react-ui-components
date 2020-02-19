ff55e08e8112f219836923dc8401b2eadf828efe
Martin Hradil <mhradil@redhat.com>
2020-02-19 12:36:43 +0000

    Move tagging to ui-classic
    need to backport tagging separately from the rest,
    we can either add react-ui-components release branches,
    or move the component to ui-classic.
    
    Doing the latter since that's the only place it's used.

--- src/tagging/index.js
+++
@@ -1,8 +0,0 @@
-import taggingApp from './reducers';
-import { TaggingConnected, TaggingWithButtonsConnected } from './containers/tagging';
-import TagView from './components/InnerComponents/TagView';
-import './sass/style.scss';
-
-export { taggingApp, TaggingConnected, TaggingWithButtonsConnected, TagView };
-export * from './components/Tagging/Tagging';
-export * from './actions';
