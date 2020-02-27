c63992b9195b76fc9b530571a4f8657caa4d3cdd
Martin Hradil <mhradil@redhat.com>
2020-02-27 13:44:15 +0000

    Remove unused rbac-forms, +helpers
    helpers/ had only one helper - intersectionWith,
    only used in rbacAssignUserTags, removed here

--- src/rbac-forms/index.js
+++
@@ -1,4 +0,0 @@
-export { default as RbacUserForm } from './rbacUserForm';
-export { default as RbacUserPreview } from './rbacUserPreview';
-export { default as RbacUserTagsList } from './rbacUserTagsList';
-export { default as RbacAssignCompanyTags } from './rbacAssignUserTags';
