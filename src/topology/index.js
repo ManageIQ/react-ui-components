4e6c0f61bfa84d8b3e6bbc81f84e01bf48e6b529
Martin Hradil <mhradil@redhat.com>
2020-02-27 15:49:18 +0000

    Remove unused topology
    used to be imported by the now defunct topologicl inventory ui

--- src/topology/index.js
+++
@@ -1,1 +0,0 @@
-export { default as TopologyCanvas } from './topologyCanvas';
--- src/topology/stories/topology.stories.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
-import TopologyWrapper from './topologyWrapper';
-
-storiesOf('Topology', module).add('Controlled topology component', withInfo()(() => (
-  <TopologyWrapper />
-)));
