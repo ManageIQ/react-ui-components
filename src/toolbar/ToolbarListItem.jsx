be14ea45b617bd1546cd4d2217a640d9f4bb031b
Kavya Nekkalapu <knekkal@us.ibm.com>
2020-10-23 17:28:41 -0400

    Fixing keydown for toolbar button

--- src/toolbar/ToolbarListItem.jsx
+++ src/toolbar/ToolbarListItem.jsx
@@ -3,20 +3,25 @@ import PropTypes from 'prop-types';
 import { MenuItem } from 'patternfly-react';
 import { ToolbarClick } from './ToolbarClick';
 
-export const ToolbarListItem = props => (
-  props.type === 'separator' ?
-    <MenuItem key={props.id} disabled={!props.enabled} eventKey={props.id} divider /> :
-    <MenuItem
-      key={props.id}
-      disabled={!props.enabled}
-      eventKey={props.id}
-      onClick={props.onClick && props.enabled ? (() => props.onClick(props)) : null}
-      onSelect={props.onSelect}
-      onKeyDown={props.onKeyDown}
-    >
-      <ToolbarClick {...props} />
-    </MenuItem>
-);
+export const ToolbarListItem = (props) => {
+  const tryKeyDown = (event) => {
+    console.log('event', event,props);
+  };
+  return (
+    props.type === 'separator' ?
+      <MenuItem key={props.id} disabled={!props.enabled} eventKey={props.id} divider /> :
+      <MenuItem
+        key={props.id}
+        disabled={!props.enabled}
+        eventKey={props.id}
+        onClick={props.onClick && props.enabled ? (() => props.onClick(props)) : null}
+        onSelect={props.onSelect}
+        // onKeyDown={aaa}
+      >
+        <ToolbarClick {...props} />
+      </MenuItem>
+  );
+};
 
 ToolbarListItem.propTypes = {
   id: PropTypes.string.isRequired,
@@ -24,10 +29,10 @@ ToolbarListItem.propTypes = {
   type: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
   onSelect: PropTypes.func,
-  onKeyDown: PropTypes.func,
+//   onKeyDown: PropTypes.func,
 };
 
 ToolbarListItem.defaultProps = {
   onSelect: null,
-  onKeyDown: null,
+//   onKeyDown: null,
 };
